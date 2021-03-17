34f857cb6914be837efef3d4528a5ed06b78c155
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-03-17 19:44:42 +0100

    Add simple styles for ListItem

--- 02/src/components/ListWrapper/ListItem/ListItem.js
+++ 02/src/components/ListWrapper/ListItem/ListItem.js
@@ -4,12 +4,12 @@ import './ListItem.css'
 import danAbramovImage from '../../../assets/images/danabramov.jpg';
 
 const ListItem = () => (
-  <li className='listItemWrapper'>
-    <img src={danAbramovImage} alt='avatar' />
+  <li className='listItem__wrapper'>
+    <img src={danAbramovImage} alt='avatar' className='listItem__image' />
     <div>
-      <h2>Dan Abramov</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum sollicitudin odio nec iaculis. Curabitur sed libero ut orci commodo porttitor.</p>
-      <button>visit twitter page</button>
+      <h2 className='listItem__name'>Dan Abramov</h2>
+      <p className='listItem__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum sollicitudin odio nec iaculis. Curabitur sed libero ut orci commodo porttitor.</p>
+      <button className='listItem__button'>visit twitter page</button>
     </div>
   </li>
 );
