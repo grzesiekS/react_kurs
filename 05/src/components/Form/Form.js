408e68cd41edacee9d0b812d8f1ddf9ee14ce1ec
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-03-25 18:57:45 +0100

    Modify addItem function in Root component

--- 05/src/components/Form/Form.js
+++ 05/src/components/Form/Form.js
@@ -50,7 +50,7 @@ class Form extends React.Component {
             <form
               autoComplete="off"
               className={styles.form}
-              onSubmit={context.addItem}
+              onSubmit={(e) => context.addItem(e, this.state)}
             >
               <div className={styles.formOptions}>
                 <Radio
